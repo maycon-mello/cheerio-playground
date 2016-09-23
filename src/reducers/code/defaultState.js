e7d9210ee35832e1112391074b0ad7820d2ba300
maycon-mello <maycon.mello@objectedge.com>
2016-09-23 09:25:26 -0300

    Fixing typos.

--- src/reducers/code/defaultState.js
+++ src/reducers/code/defaultState.js
@@ -1,7 +1,7 @@
 export const htmlSource =
 `<!--
  Edit this code or paste some html markup here.
- The application state persist in localStorage, so you don't lost your changes
+ The application state persists in localStorage, so you don't lost your code
  after closing the browser.
 -->
 <html>
@@ -33,26 +33,26 @@ export const htmlSource =
 export const js =
 `/**
  * Edit this js to perform transformation on the html structure
- * This plagyround include helpers with functions like
+ * This playground include helpers with functions like
  * wrapTextChildren, dumpTable...
  */
 const $body = $('body');
 
-// Creating and inserting a new element on body
+// Creating and inserting a new element on the body
 const myDiv = $(tag('div', {class: 'my-div'}, 'My new Div'));
 $body.append(myDiv);
 
 let listItems = $body.find('li').addClass('test-class');
 
 // Logs example
-console.log("You can create logs to debug, to see it click in the logs button");
+console.log("Using logs to debug, to see it click in the logs button");
 console.log(myDiv);
 console.log('List items found: ', listItems.length);
 
 // Dump tables
 $body.find('table').dumpTable();
 
-// Createing functions
+// Functions usage
 function removeListItem(idx) {
  $body.find('#content li').eq(idx).remove();
 }
